dc86f173f541a55a93c5435fa83d407baf909b1d
Andres <ae.acosta94@gmail.com>
2022-09-16 17:36:07 -0500

    Se agrega el proyecto de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-simple-crud';
+
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Ryan', country: 'USA' },
+    { id: 2, name: 'Alex', country: 'UK' },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+
+  delete() {
+    if (confirm('Are you sure of this action?')) {
+      this.employeeArray = this.employeeArray.filter(
+        (x) => x != this.selectedEmployee
+      );
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
